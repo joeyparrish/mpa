4233bf91b70c1d6c563d5fa51f52185c68149e38
Joey Parrish <joey.parrish@gmail.com>
2023-08-15 19:31:55 -0500

    Fold utils into MPA
    At this point, the new implementation is 64% fewer lines of code and 70% smaller compiled code.

--- mpa/mpa.cc
+++ mpa/mpa.cc
@@ -65,8 +65,6 @@
 #include <cmath>
 #include <cstring>
 
-#include "util.h"
-
 #define COUNT 60
 #define Y_COUNT 63
 #define SUN_RADIUS 0.26667
@@ -262,6 +260,25 @@ const double MB_TERMS[COUNT][MPA_TERM_COUNT] = {
     {4, -1, 0, -1, 115, 0},   {2, -2, 0, 1, 107, 0}};
 
 int integer(double value) { return value; }
+
+double rad2deg(double radians) { return (180.0 / M_PI) * radians; }
+
+double deg2rad(double degrees) { return (M_PI / 180.0) * degrees; }
+
+double limit_degrees(double degrees) {
+  double limited;
+
+  degrees /= 360.0;
+  limited = 360.0 * (degrees - floor(degrees));
+  if (limited < 0) limited += 360.0;
+
+  return limited;
+}
+
+double third_order_polynomial(double a, double b, double c, double d,
+                              double x) {
+  return ((a * x + b) * x + c) * x + d;
+}
 
 double julian_day(int year, int month, int day, int hour, int minute,
                   double second) {
